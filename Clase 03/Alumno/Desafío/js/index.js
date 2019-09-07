1385a20cd4d8ad616c018c56578e4d92911a6181
Hernan Fidalgo <hernan.fidalgo@gmail.com>
2019-09-07 12:52:03 -0300

    actualizacion ejercicios CLASE3

---
+++ Clase 03/Alumno/Desafío/js/index.js
@@ -0,0 +1,20 @@
+var operacion = prompt('Ingresar una operacion: ')
+var numero = prompt('Ingresar un numero: ')
+var numero2 = prompt('Ingresar otro numero: ')
+
+switch(resultado) {
+    case operacion: '+', '-', '*', '/'
+      [break;]
+    case numero: <= 0
+      [break;]
+    ...
+    case numero2: <= 0
+      [break;]
+    
+  }
+  operacion = parseInt(operacion);
+  prompt(operacion + numero + numero2 = resultado)
+
+// while(numero <= 0 && numero2 <= 0){
+//     var resultado = operacion + numero + numero2
+// }
