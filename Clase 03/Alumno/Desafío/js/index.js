b2541c2e1b3a0ef40459ed06c2609fd752b9ccdf
Hernan Fidalgo <hernan.fidalgo@gmail.com>
2019-09-07 15:43:20 -0300

    actualizacion con desafio CLASE02

--- Clase 03/Alumno/Desafío/js/index.js
+++ Clase 03/Alumno/Desafío/js/index.js
@@ -1,24 +1,25 @@
 var operacion = prompt('Ingresar una operacion: ')  
-var numero = prompt('Ingresar un numero: ')
-var numero2 = prompt('Ingresar otro numero: ')
+var numero = parseInt(prompt('Ingresar un numero: '))
+var numero2 = parseInt(prompt('Ingresar otro numero: '))
 
-switch(resultado) {
-    case operacion: '+'; 
-       [break;]
-    case operacion: '-';
-       [break;]
-    case operacion: '*';
-       [break;]
-    case operacion: '/';
-       [break;]
-    case numero: <= 0
-       [break;]
-    case numero2: <= 0
-       [break;]
+var resultado = 0
+
+switch(operacion) {
+    case '+': 
+   resultado = numero + numero2
+       break;
+    case '-':
+   resultado = numero - numero2
+       break;
+    case '*':
+   resultado = numero * numero2
+       break;
+    case '/':
+   resultado = numero / numero2
+       break;
+   default: 
+   break;
    }
 
-   print = resultado{
-   operacion = parseInt(operacion);
-
-   alert(operacion + numero + numero2)
-                    }
+alert(resultado)
+document.getElementById("id").innerHTML = resultado;
