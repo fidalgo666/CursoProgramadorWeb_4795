91c06964f54c5fdec0071c0429115a14a4ca031a
Hernan Fidalgo <hernan.fidalgo@gmail.com>
2019-09-07 15:01:10 -0300

    actualizacion por la tarde

--- Clase 03/Alumno/Desafío/js/index.js
+++ Clase 03/Alumno/Desafío/js/index.js
@@ -1,20 +1,24 @@
-var operacion = prompt('Ingresar una operacion: ')
+var operacion = prompt('Ingresar una operacion: ')  
 var numero = prompt('Ingresar un numero: ')
 var numero2 = prompt('Ingresar otro numero: ')
 
 switch(resultado) {
-    case operacion: '+', '-', '*', '/'
-      [break;]
+    case operacion: '+'; 
+       [break;]
+    case operacion: '-';
+       [break;]
+    case operacion: '*';
+       [break;]
+    case operacion: '/';
+       [break;]
     case numero: <= 0
-      [break;]
-    ...
+       [break;]
     case numero2: <= 0
-      [break;]
-    
-  }
-  operacion = parseInt(operacion);
-  prompt(resultado = operacion + numero + numero2)
+       [break;]
+   }
 
-// while(numero <= 0 && numero2 <= 0){
-//     var resultado = operacion + numero + numero2
-// }
+   print = resultado{
+   operacion = parseInt(operacion);
+
+   alert(operacion + numero + numero2)
+                    }
