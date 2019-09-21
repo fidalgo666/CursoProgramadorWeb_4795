ec628e32e9595f2104b43a5854e0d8dfd95f8e74
Hernan Fidalgo <hernan.fidalgo@gmail.com>
2019-09-21 16:26:37 -0300

    actualizacion por desasfio

---
+++ Clase 08/Alumno/Desafío/js/index.js
@@ -0,0 +1,19 @@
+var studentNode = createStudentNode(newStudent)
+
+function nuevoElemento(student){
+    var nuevoElemento = {
+        firstName: 'Juan',
+        lastName: 'Peréz',
+        dni: 22999333,
+        email: 'juan@gmail.com'
+      }
+
+      var student
+      var student = document.createElement('li')
+      var student = document.createElement('h3')
+      var student = document.createElement('p')
+
+      nuevoElemento.appendChild(listItemNode)
+}
+
+
