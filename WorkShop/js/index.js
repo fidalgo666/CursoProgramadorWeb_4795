8de7c109cca123f205f9a30a1adfcac7b6ca867f
Hernan Fidalgo <hernan.fidalgo@gmail.com>
2019-10-05 16:00:08 -0300

    actualizacion del mediodia3

--- WorkShop/js/index.js
+++ WorkShop/js/index.js
@@ -1,13 +1,10 @@
   function validarCampoFormulario(evento){
-
     // estoy recuperando el elemento HTML desde el cual se disparo el evento
     var elemento = evento.target
-
     // obtengo el valor del campo del formulario
     var valorDelCampo = elemento.value
 
     if(valorDelCampo != '' ){
-
         // se agrega clase is-valid de bootstrap 
         elemento.classList.add('is-valid')
         elemento.classList.remove('is-invalid')
@@ -19,15 +16,12 @@
 }
 
 function validarCampoEmail(evento){
-
   // estoy recuperando el elemento HTML desde el cual se disparo el evento
   var elemento = evento.target
-
   // obtengo el valor del campo del formulario
   var valorDelCampo = elemento.value
 
   if(valorDelCampo.includes("@") && valorDelCampo.includes(".") && valorDelCampo != ''){
-
       // se agrega clase is-valid de bootstrap 
       elemento.classList.add('is-valid')
       elemento.classList.remove('is-invalid')
@@ -39,15 +33,12 @@ function validarCampoEmail(evento){
 }
 
 function validarCampoNumero(evento){
-
   // estoy recuperando el elemento HTML desde el cual se disparo el evento
   var elemento = evento.target
-
   // obtengo el valor del campo del formulario
   var valorDelCampo = elemento.value
 
   if(valorDelCampo != -1 && valorDelCampo != ''){
-
       // se agrega clase is-valid de bootstrap 
       elemento.classList.add('is-valid')
       elemento.classList.remove('is-invalid')
@@ -59,31 +50,34 @@ function validarCampoNumero(evento){
 }
 
 var campoNombre = document.getElementById('validationCustom01')
-
 campoNombre.onblur = validarCampoFormulario
 
 var campoNombre = document.getElementById('validationCustom02')
-
 campoNombre.onblur = validarCampoFormulario
 
 var campoEmail = document.getElementById('validationCustom04')
-
 campoEmail.onblur = validarCampoEmail
 
 var campoNumero = document.getElementById('validationCustom03')
+campoNumero.onblur = validarCampoNumeroHandler
 
-campoNumero.onblur = validarCampoNumero
+function validarCampoNumeroHandler (evento){
+    var elemento = evento.target
+    var valorDelCampo = elemento.value
+    if(valorDelCampo !='' && valorDelCampo >0){
+        elemento.classList.add('is-valid')
+        elemento.classList.remove('is-invalid')
+    } else{
+        elemento.classList.add('is-invalid')
+        elemento.classList.remove('is-valid')
+    }
+}
 
-object.onclick = function(eventoClick){
-  
-};
 
+var campoAgregoAlumnoBTN = document.getElementById('boton')
+campoAgregoAlumnoBTN.onclick = showMessage
 
-// function botonHabilitado() {
-//   if(validationCustom01('is-valid') && validationCustom02('is-valid') && validationCustom03('is-valid') && validationCustom04('is-valid')){
-//     document.getElementById('boton').disabled = false
-//   }  else {
-//     document.getElementById('boton').disabled = true
-//   }
-// }
-
+function showMessage(event){
+    var inputNode =  event.target
+    
+}
