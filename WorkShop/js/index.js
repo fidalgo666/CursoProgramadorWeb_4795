d36ab712d62dfd4f4405a02a81df020da8037006
Hernan Fidalgo <hernan.fidalgo@gmail.com>
2019-10-05 10:09:01 -0300

    actualizacion semanal

--- WorkShop/js/index.js
+++ WorkShop/js/index.js
@@ -1,14 +1,89 @@
-(function() {
-    'use strict';
+  function validarCampoFormulario(evento){
+
+    // estoy recuperando el elemento HTML desde el cual se disparo el evento
+    var elemento = evento.target
+
+    // obtengo el valor del campo del formulario
+    var valorDelCampo = elemento.value
+
+    if(valorDelCampo != '' ){
+
+        // se agrega clase is-valid de bootstrap 
+        elemento.classList.add('is-valid')
+        elemento.classList.remove('is-invalid')
+    } else{
+        // se agrega clase is-invalid de bootstrap 
+        elemento.classList.add('is-invalid')
+        elemento.classList.remove('is-valid')
+    }
+}
+
+function validarCampoEmail(evento){
+
+  // estoy recuperando el elemento HTML desde el cual se disparo el evento
+  var elemento = evento.target
+
+  // obtengo el valor del campo del formulario
+  var valorDelCampo = elemento.value
+
+  if(valorDelCampo.includes("@") && valorDelCampo.includes(".") && valorDelCampo != ''){
+
+      // se agrega clase is-valid de bootstrap 
+      elemento.classList.add('is-valid')
+      elemento.classList.remove('is-invalid')
+  } else{
+      // se agrega clase is-invalid de bootstrap 
+      elemento.classList.add('is-invalid')
+      elemento.classList.remove('is-valid')
+  }
+}
+
+function validarCampoNumero(evento){
+
+  // estoy recuperando el elemento HTML desde el cual se disparo el evento
+  var elemento = evento.target
+
+  // obtengo el valor del campo del formulario
+  var valorDelCampo = elemento.value
+
+  if(valorDelCampo != -1 && valorDelCampo != ''){
+
+      // se agrega clase is-valid de bootstrap 
+      elemento.classList.add('is-valid')
+      elemento.classList.remove('is-invalid')
+  } else{
+      // se agrega clase is-invalid de bootstrap 
+      elemento.classList.add('is-invalid')
+      elemento.classList.remove('is-valid')
+  }
+}
+
+var campoNombre = document.getElementById('validationCustom01')
+
+campoNombre.onblur = validarCampoFormulario
+
+var campoNombre = document.getElementById('validationCustom02')
+
+campoNombre.onblur = validarCampoFormulario
+
+var campoEmail = document.getElementById('validationCustom04')
+
+campoEmail.onblur = validarCampoEmail
+
+var campoNumero = document.getElementById('validationCustom03')
+
+campoNumero.onblur = validarCampoNumero
+
+object.onclick = function(eventoClick){
   
-    window.addEventListener('load', function() {
-      var form = document.getElementById('needs-validation');
-      form.addEventListener('submit', function(event) {
-        if (form.checkValidity() === false) {
-          event.preventDefault();
-          event.stopPropagation();
-        }
-        form.classList.add('was-validated');
-      }, false);
-    }, false);
-  })();
+};
+
+
+// function botonHabilitado() {
+//   if(validationCustom01('is-valid') && validationCustom02('is-valid') && validationCustom03('is-valid') && validationCustom04('is-valid')){
+//     document.getElementById('boton').disabled = false
+//   }  else {
+//     document.getElementById('boton').disabled = true
+//   }
+// }
+
