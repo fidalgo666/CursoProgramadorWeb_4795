f501864d2c8b34835892869b955a10da9c1e8342
Hernan Fidalgo <hernan.fidalgo@gmail.com>
2019-11-02 14:22:56 -0300

    otra actualizacion mas

---
+++ Clase 14/Alumno/js/index.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    $('.figure-img').on('click', function(){
+         var elementoIMGqueEstaPasandoElEvento = $(this);
+         elementoIMGqueEstaPasandoElEvento.fadeOut(2500);
+    (function(){
+            remove()
+})
+})
+})
+
+// misma funcion pero suscribiendose a la clase del elemento (para cuando son muchas img)
+
+// $(document).ready(function () {
+//     $('.figure-img').on('click', function(){
+//         var elementoIMGqueEstaPasandoElEvento = $(this);
+//         elementoIMGqueEstaPasandoElEvento.fadeOut(2500);
+//     })
+//     })
