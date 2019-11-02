87ce432f76f80768a8486707bde243a540006063
Hernan Fidalgo <hernan.fidalgo@gmail.com>
2019-11-02 15:06:43 -0300

    otra actualizacion de Sabado x la tarde

---
+++ Clase 14/Alumno/ejercicioClase/js/index.js
@@ -0,0 +1,18 @@
+var request = $.ajax({
+    url: "https://swapi.co/api/people/" + prompt("ingrese el numero de su personaje: "),
+    method: "GET"
+  })
+
+ request.done(function( data ) {
+    $("#name").val(data.name); // cuando se trata de ingresar texto a un "input" se usa .val (cuando se quiere ingresar a un div u otro va, ".html")
+  });
+  
+ request.done(function( data ) {
+    $("#gender").val(data.gender); // cuando se trata de ingresar texto a un "input" se usa .val (cuando se quiere ingresar a un div u otro va, ".html")
+  });
+
+
+// ESTO PARA RESPONDER DESDE LA API UN "ERROR"
+//   request.fail(function( error ) {
+//     console.log( 'Error: ' , error )
+//   })
