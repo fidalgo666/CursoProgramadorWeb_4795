1f033898d875035090c45989df0eb2d1199a6df4
Augusto Giacopuzzi <augusto@goalsetter.co>
2019-11-02 15:16:30 -0300

    En clase

--- Clase 14/Profe/En clase/Efectos/js/index.js
+++ Clase 14/Profe/En clase/Efectos/js/index.js
@@ -1,9 +1,37 @@
 $(function () {
 
+  $(window).on('click', function () {
+    alert('click')
+  })
+
   //OCULTAR EL ELEMENTO CON ID COVER1
-  $('#cover1').slideUp(3000, function () {
-    $('#cover1').slideDown(3000, function () {
-      alert('EL ELEMENTO AHORA SE VE DE NUEVO')
-    });
+  // $('#cover1').slideUp(3000, function () {
+  //   $('#cover1').slideDown(3000, function () {
+  //     alert('EL ELEMENTO AHORA SE VE DE NUEVO')
+  //   });
+  // })
+
+  $('.figure-img').on('click', function () {
+
+    var elementoQueEstaPasandoElEvento = $(this);
+
+    elementoQueEstaPasandoElEvento.fadeOut(2000);
+
+  })
+
+  $('.figure-img').on('mouseover', function () {
+
+    var elementoQueEstaPasandoElEvento = $(this);
+
+    elementoQueEstaPasandoElEvento.hide();
+
+  })
+
+  $('.figure-img').click(function () {
+
+    var elementoQueEstaPasandoElEvento = $(this);
+
+    elementoQueEstaPasandoElEvento.fadeOut(2000);
+
   })
 })
---
+++ Clase 14/Profe/En clase/Parallax/js/index.js
@@ -0,0 +1,8 @@
+$(window).scroll(function () {
+  var scrollTop = $(window).scrollTop()
+  $('#background1').css('top', -(scrollTop * 0.25) + 'px')
+  $('#background2').css('top', -(scrollTop * 0.75) + 'px')
+  $('#background3').css('top', -(scrollTop * 2) + 'px')
+  console.log('ESTOY SCROLLEANDO!')
+})
+
